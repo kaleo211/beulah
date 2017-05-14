084e05bdd90598784e309b19faec03a5b053a436
Xuebin He <xuebin.he@emc.com>
2017-05-14 09:16:06 -0400

    Import javascript files from node modules

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 
 app.use('/', router);
 app.use(express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/node_modules'));
 
 app.set('views', __dirname + '/views');
 
